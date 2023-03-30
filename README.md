83f6dc6a47d1b17b934649c4683fc118b6c516b4
Drew Foehn <drew.foehn@publicissapient.com>
2023-03-30 17:40:42 -0400

    feat: output build info to json bin and basic middleware

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
-## create build info
+## output build info
 
-Shell tool to output ci-info from GHA, JENKINS, TRAVIS_CI falling back to git info into json file. Includes express middleware for output
+Shell tool to write ci-info from GHA, JENKINS, TRAVIS_CI info into json file. Includes express middleware helpers to output
 
 ## usage:
 
+output .buildinfo.json to root of package
+
 ```shell
-$ npx @pixeldrew/create-build-info
+$ npx @pixeldrew/output-build-info
+Wrote build-info to /app/.buildinfo.json
 ```
 
 ```javascript
+import express from "express";
 import {
-  addBuildinfo,
+  addBuildInfo,
   showBuildInfo,
-} from "@pixeldrew/create-build-info/middleware";
+} from "@pixeldrew/output-build-info/middleware.js";
 
-app.use(addBuildinfo());
-app.get("/.version-info", showBuildInfo);
+const app = new express();
+
+app.use(addBuildInfo());
+app.get("/.build-info", showBuildInfo);
+
+app.get("/build", (req, res) => {
+  const buildInfo = req.app.get("build-info");
+
+  res.send(`${buildInfo.version} ${buildInfo.name} ${buildInfo.sha}`);
+});
+
+app.listen(3000, () => {
+  console.log(`I'm http on port 3000`);
+});
 ```
---
+++ bin/output-build-info.js
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+/**
+ * Outputs VCS & CI info to json file
+ * first tries GITHUB_SHA, then GIT_COMMIT then if neither of those exist it tries git
+ */
+import { writeFile } from "fs/promises";
+import { sep } from "path";
+import { execFileSync } from "child_process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { DEFAULT_FILENAME } from "../middleware.js";
+
+const git = (arg) =>
+  execFileSync(`git`, arg.split(" ")).toString("utf8").trim();
+const { env } = process;
+
+const argv = yargs(hideBin(process.argv))
+  .alias("f", "file")
+  .default("f", DEFAULT_FILENAME)
+  .describe("f", "Output filename")
+  .alias("d", "directory")
+  .default("d", process.cwd())
+  .describe("f", "Output directory")
+  .help("h")
+  .alias("h", "help")
+  .usage("Usage: $0 -f [filename] -d [directory]").argv;
+
+const filename = `${argv.directory}${sep}${argv.file}`;
+const sha =
+  env.GITHUB_SHA ??
+  env.GIT_COMMIT ??
+  env.TRAVIS_COMMIT ??
+  git("rev-parse HEAD");
+const branch =
+  env.GITHUB_REF_NAME ??
+  env.GIT_BRANCH ??
+  env.TRAVIS_BRANCH ??
+  git(`rev-parse --abbrev-ref HEAD`);
+const build =
+  env.GITHUB_JOB ?? env.BUILD_NUMBER ?? env.TRAVIS_BUILD_NUMBER ?? "UNKNOWN";
+
+try {
+  await writeFile(
+    filename,
+    JSON.stringify(
+      {
+        sha,
+        branch,
+        build,
+      },
+      null,
+      4
+    )
+  );
+
+  console.log(`Wrote build-info to ${filename}`);
+} catch (e) {
+  console.error(`could not write ${filename} ${e}`);
+}
